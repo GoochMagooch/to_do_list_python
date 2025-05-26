ce30e0ea08d795aa1870f5ea745f43230e2413f2
Magooch <crvallecito@gmail.com>
2025-05-26 01:44:01 -0700

    Refactor rest of functions to read from and write to external file

--- todo.py
+++ todo.py
@@ -42,80 +42,89 @@ def add_task():
         for i in range(len(tasks)):
             x.write(tasks[i])
 
-'''
 def remove_task():
+    with open("todo_list.csv", "r") as x:
+        y = x.readlines()
+    
+    nums = []
+    for i in range(len(y)):
+        nums.append(i+1)
+
     loop = True
-    if len(tasks) < 1:
+    if len(nums) < 1:
         print("No tasks to remove! Add some by pressing 1")
     else:
         while loop:
             print("Your Tasks:")
-            task_num = 1
             numbered_tasks = []
-            for i in tasks:
-                numbered_tasks.append(f"{task_num}. {i}")
-                print(numbered_tasks[-1])
-                task_num += 1
+            for i in range(len(y)):
+                numbered_tasks.append(f"{nums[i]}. {y[i]}")
+                print(numbered_tasks[-1].strip("\n"))
                 # Placing the print statement after the remove method removed an item in tasks
                 # before the appropriate print statement could be run, resulting in indexing issues
             task_remove = input("Choose the task to remove by number or \"back\": ")
             if task_remove.lower() == "back":
-                display_menu(0)
+                display_menu()
                 loop = False
             else:
                 for i in range(len(numbered_tasks)):
                     if task_remove == numbered_tasks[i][0]:
                         clear_screen()
-                        print(f"'{tasks[i]}' has been removed from your task list!")
-                        tasks.remove(tasks[i])
+                        print(f"'{y[i].strip()}' has been removed from your task list!")
+                        y.remove(y[i])
                         loop = False
                     else:
                         clear_screen()
                         print("Invalid choice, choose a task number!")
                         pass
-'''
+    with open("todo_list.csv", "w") as x:
+        for i in range(len(y)):
+            x.write(y[i])
       
 def list_tasks():
+    print("YOUR TASKS:")
     with open("todo_list.csv", "r") as x:
-        y = x.read().split("\n")
+        y = x.readlines()
     for i in range(len(y)):
         if y[i] == "":
             continue
         else:
-            print(y[i])
+            print(str(i+1) + ". " + y[i].strip())
+    print()
 
-'''
 def check_off_task():
+
+    with open("todo_list.csv", "r") as x:
+        y = x.readlines()
+
     loop = True
-    if len(tasks) < 1:
+    if len(y) < 1:
         print("No tasks to check off! Add some by pressing 1")
     else:
         while loop:
             print("Tasks:")
-            task_num = 1
             numbered_tasks = []
-            for i in tasks:
-                numbered_tasks.append(f"{task_num}. {i}")
+            for i in range(len(y)):
+                numbered_tasks.append(f"{i+1}. {y[i]}")
                 print(numbered_tasks[-1])
-                task_num += 1
             nums = []
             for i in numbered_tasks:
                 nums.append(i[0])
             task_checkoff = input("Choose the task to checkoff by number or \"back\": ")
             if task_checkoff.lower() == "back":
-                display_menu(0)
+                display_menu()
                 loop = False
             elif task_checkoff in nums:
                 for i in range(len(numbered_tasks)):
                     if task_checkoff == numbered_tasks[i][0]:
-                        if "✅" in tasks[i]:
+                        if "✅" in y[i].strip():
                             clear_screen()
                             print("This task is already checked off, great job!")
                             pass
-                        elif not "✅" in tasks[i]:
+                        elif not "✅" in y[i].strip():
                             clear_screen()
-                            print(f"Congratulations on completing your '{tasks[i]}' task!")
-                            tasks[i] = tasks[i] + " ✅"
+                            print(f"Congratulations on completing your '{y[i]}' task!")
+                            y[i] = y[i].strip() + " ✅\n"
                             loop = False
                         else:
                             clear_screen()
@@ -125,11 +134,12 @@ def check_off_task():
                 clear_screen()
                 print("Invalid choice, choose a task number!")
                 pass
-
-display_menu()
-'''
+    with open("todo_list.csv", "w") as x:
+        for i in range(len(y)):
+            x.write(y[i])
 
 while True:
+    display_menu()
     choice = input("Choose an option (1 - 4), \"Exit\" or \"Menu\": ")
     if choice.lower() == "exit":
         clear_screen()
@@ -145,10 +155,10 @@ while True:
                 add_task()
             elif int_choice == 2:
                 clear_screen()
-                #check_off_task()
+                check_off_task()
             elif int_choice == 3:
                 clear_screen()
-                #remove_task()
+                remove_task()
             elif int_choice == 4:
                 clear_screen()
                 list_tasks()
